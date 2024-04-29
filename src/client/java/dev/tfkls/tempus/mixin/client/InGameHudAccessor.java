b61668bb5554ebb84a0a58c2df97c35468d0bc42
Tomasz "TFKls" Kulis <pub@tfkls.dev>
2024-04-29 22:52:51 +0200

    Added rendering for thirst toolbar (w/o icons, currently uses hunger icons, which shows as two hunger bars)

---
+++ src/client/java/dev/tfkls/tempus/mixin/client/InGameHudAccessor.java
@@ -0,0 +1,18 @@
+package dev.tfkls.tempus.mixin.client;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(InGameHud.class)
+public interface InGameHudAccessor {
+    @Accessor
+    MinecraftClient getClient();
+    @Accessor
+    Random getRandom();
+    @Accessor
+    Identifier getICONS();
+}
---
+++ src/client/java/dev/tfkls/tempus/mixin/client/PlayerThirstHudMixin.java
@@ -0,0 +1,63 @@
+package dev.tfkls.tempus.mixin.client;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.registry.tag.FluidTags;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public abstract class PlayerThirstHudMixin {
+    @Shadow protected abstract PlayerEntity getCameraPlayer();
+
+    @Shadow protected abstract int getHeartRows(int heartCount);
+
+    // TODO: Change icon provider to water drops (add custom textures)
+    @Unique
+    private static final int emptyDropIcon = 16;
+    @Unique
+    private static final int halfDropIcon = 16+36+25;
+    @Unique
+    private static final int fullDropIcon = 16+36;
+
+    @Inject(method = "renderStatusBars", at = @At(value = "INVOKE_ASSIGN", target = "Ljava/lang/Math;min(II)I"), cancellable = true)
+    void renderThirstBar(DrawContext context, CallbackInfo ci, @Local(name = "t") LocalIntRef renderHeightRef, @Local(name = "n") int renderWidth, @Local(name = "x") int mountHeartCount) {
+        MinecraftClient client = ((InGameHudAccessor) this).getClient();
+        Random random = ((InGameHudAccessor) this).getRandom();
+        Identifier icons = ((InGameHudAccessor) this).getICONS();
+        PlayerEntity player = this.getCameraPlayer();
+        int renderHeight = renderHeightRef.get() - (this.getHeartRows(mountHeartCount)-1)*10; // This is copied from the "air" rendering
+
+        client.getProfiler().swap("thirst");
+        if (player.isSubmergedIn(FluidTags.WATER) || player.getAir() < player.getMaxAir()) return; // This ensures we don't clash with air indicators
+
+        int currentThirst = 15; // TODO: Add logic to show actual thirst levels (after server-side implementation)
+        float currentHydration = 0.5f;
+
+        for (int loc = 0; loc < 10; loc++) {
+            int verticalPosition = renderHeight;
+            if (currentHydration <= 0.0f) {
+                verticalPosition += random.nextInt(3)-1;
+            }
+            int horizontalPosition = renderWidth - 8*loc - 9;
+            context.drawTexture(icons, horizontalPosition, verticalPosition, emptyDropIcon, 27, 9, 9);
+            if (loc*2+1 < currentThirst) {
+                context.drawTexture(icons, horizontalPosition, verticalPosition, fullDropIcon, 27, 9, 9);
+            }
+            if (loc*2+1 == currentThirst) {
+                context.drawTexture(icons, horizontalPosition, verticalPosition, halfDropIcon, 27, 9, 9);
+            }
+        }
+        renderHeightRef.set(renderHeight);
+    }
+}
