2a25c45e3d72644a1650715f2c50814a2196760e
Surowizator <46932793+Surowizator@users.noreply.github.com>
2024-05-26 04:46:09 +0200

    Added purity logic to PotionItem

--- src/main/java/dev/tfkls/tempus/mixin/PotionItemThirstMixin.java
+++ src/main/java/dev/tfkls/tempus/mixin/PotionItemThirstMixin.java
@@ -3,6 +3,8 @@ package dev.tfkls.tempus.mixin;
 import dev.tfkls.tempus.core.DrinkComponent;
 import dev.tfkls.tempus.core.ThirstManager;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.PotionItem;
 import net.minecraft.world.World;
@@ -17,5 +19,11 @@ public abstract class PotionItemThirstMixin {
     @Inject(method = "finishUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;incrementStat(Lnet/minecraft/stat/Stat;)V"))
     public void injectFinishUsing(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
         ((ThirstManager.MixinAccessor)user).tempus$getThirstManager().drink((DrinkComponent.MixinAccessor) this);
+        if (!world.isClient() && !((DrinkComponent.MixinAccessor) this).tempus$getDrinkComponent().isPurified() && stack.getName().getString().equals("Water Bottle")) {
+            if (Math.random()>0.5) {
+                user.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 3*20));
+            }
+        }
+
     }
 }
