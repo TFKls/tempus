5c68fa449f3ce38bfa168c28a35df07cbab234be
Surowizator <46932793+Surowizator@users.noreply.github.com>
2024-06-10 01:25:32 +0200

    Added item model generation structure and thermometer texture

--- src/main/java/dev/tfkls/tempus/TempusDataGenerator.java
+++ src/main/java/dev/tfkls/tempus/TempusDataGenerator.java
@@ -1,5 +1,6 @@
 package dev.tfkls.tempus;
 
+import dev.tfkls.tempus.datagen.CustomItemModelProvider;
 import dev.tfkls.tempus.datagen.DrinkableItemsRecipeProvider;
 import dev.tfkls.tempus.datagen.VanillaNutritionTagProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
@@ -11,5 +12,6 @@ public class TempusDataGenerator implements DataGeneratorEntrypoint {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 		pack.addProvider(VanillaNutritionTagProvider::new);
 		pack.addProvider(DrinkableItemsRecipeProvider::new);
+		pack.addProvider(CustomItemModelProvider::new);
 	}
 }
