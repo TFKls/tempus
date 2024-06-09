8a3ea32d2ac2136141f6b92c85f702969f3b98c5
Surowizator <46932793+Surowizator@users.noreply.github.com>
2024-06-09 21:02:59 +0200

    Proper display for thirst with icons

--- src/client/java/dev/tfkls/tempus/mixin/client/InGameHudThirstMixin.java
+++ src/client/java/dev/tfkls/tempus/mixin/client/InGameHudThirstMixin.java
@@ -2,6 +2,7 @@ package dev.tfkls.tempus.mixin.client;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
+import dev.tfkls.tempus.effects.CustomStatusEffects;
 import dev.tfkls.tempus.managers.ThirstManager;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
@@ -25,19 +26,20 @@ public abstract class InGameHudThirstMixin {
 	@Shadow
 	protected abstract int getHeartRows(int heartCount);
 
+	@Shadow private int ticks;
 	// TODO: Change icon provider to water drops (add custom textures)
 	@Unique
-	private static final int emptyDropIcon = 16;
+	private static final int emptyDropIcon = 0;
 	@Unique
-	private static final int halfDropIcon = 16 + 45;
+	private static final int fullDropIcon = 9;
 	@Unique
-	private static final int fullDropIcon = 16 + 36;
+	private static final int halfDropIcon = 18;
 
 	@Inject(method = "renderStatusBars", at = @At(value = "INVOKE_ASSIGN", target = "Ljava/lang/Math;min(II)I"))
 	void renderThirstBar(DrawContext context, CallbackInfo ci, @Local(name = "t") LocalIntRef renderHeightRef, @Local(name = "n") int renderWidth, @Local(name = "x") int mountHeartCount) {
 		MinecraftClient client = ((InGameHudAccessor) this).getClient();
 		Random random = ((InGameHudAccessor) this).getRandom();
-		Identifier icons = ((InGameHudAccessor) this).getICONS();
+		Identifier icons = new Identifier("tempus", "icons.png");
 		PlayerEntity player = this.getCameraPlayer();
 		int renderHeight = renderHeightRef.get() - (this.getHeartRows(mountHeartCount) - 1) * 10; // This is copied from the "air" rendering
 
@@ -46,20 +48,21 @@ public abstract class InGameHudThirstMixin {
 			return; // This ensures we don't clash with air indicators
 
 		int currentThirst = ((ThirstManager.MixinAccessor) player).tempus$getThirstManager().getThirst();
-		float currentHydration = 0.5f;
+		int offset = 0;
+		if (player.hasStatusEffect(CustomStatusEffects.THIRST)) offset = 27;
 
 		for (int loc = 0; loc < 10; loc++) {
 			int verticalPosition = renderHeight;
-			if (currentHydration <= 0.0f) {
+			if (currentThirst <= 10 && this.ticks % (3*currentThirst+1) == 0) {
 				verticalPosition += random.nextInt(3) - 1;
 			}
 			int horizontalPosition = renderWidth - 8 * loc - 9;
-			context.drawTexture(icons, horizontalPosition, verticalPosition, emptyDropIcon, 27, 9, 9);
+			context.drawTexture(icons, horizontalPosition, verticalPosition, emptyDropIcon+offset, 0, 9, 9);
 			if (loc * 2 + 1 < currentThirst) {
-				context.drawTexture(icons, horizontalPosition, verticalPosition, fullDropIcon, 27, 9, 9);
+				context.drawTexture(icons, horizontalPosition, verticalPosition, fullDropIcon+offset, 0, 9, 9);
 			}
 			if (loc * 2 + 1 == currentThirst) {
-				context.drawTexture(icons, horizontalPosition, verticalPosition, halfDropIcon, 27, 9, 9);
+				context.drawTexture(icons, horizontalPosition, verticalPosition, halfDropIcon+offset, 0, 9, 9);
 			}
 		}
 		renderHeightRef.set(renderHeight);
