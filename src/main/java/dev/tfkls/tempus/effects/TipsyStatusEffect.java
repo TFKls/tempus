4eab10acf0910b2021ea5b7b67f9cc91a05d566f
k-kosiorowski <krzysztof.kosiorowski@student.uj.edu.pl>
2024-06-10 01:45:36 +0200

    Added tipsy status effect

--- src/main/java/dev/tfkls/tempus/effects/TipsyStatusEffect.java
+++ src/main/java/dev/tfkls/tempus/effects/TipsyStatusEffect.java
@@ -6,15 +6,19 @@ import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class TipsyStatusEffect extends StatusEffect {
 
-    public TipsyStatusEffect() {
-        super(StatusEffectCategory.HARMFUL, 0xffffff);
-    }
+	public TipsyStatusEffect() {
+		super(StatusEffectCategory.HARMFUL, 0xffffff);
+	}
 
-    @Override
-    public boolean canApplyUpdateEffect(int duration, int amplifier) {
-        return false;
-    }
+	@Override
+	public void onApplied(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+		if (entity instanceof PlayerEntity player) {
+			if (player.hasStatusEffect(CustomStatusEffects.TIPSY))
+				player.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 20 * amplifier));
+		}
+	}
 }
