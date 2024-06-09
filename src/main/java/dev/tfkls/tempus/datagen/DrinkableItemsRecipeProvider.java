86b80664b89007e2dab746fcdf8bb63106574983
Tomasz "TFKls" Kulis <pub@tfkls.dev>
2024-06-09 22:20:51 +0200

    Reformatted the ItemsProvider

--- src/main/java/dev/tfkls/tempus/datagen/DrinkableItemsRecipeProvider.java
+++ src/main/java/dev/tfkls/tempus/datagen/DrinkableItemsRecipeProvider.java
@@ -22,9 +22,37 @@ public class DrinkableItemsRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public void generate(Consumer<RecipeJsonProvider> exporter) {
-        RecipeProvider.offerSmelting(exporter, List.of(Items.POTION), RecipeCategory.FOOD, DrinkableItems.PURIFIED_WATER_BOTTLE, 0.1f, 200, "tempus");
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.FOOD, DrinkableItems.WHEAT_EXTRACT, 1).input(DrinkableItems.PURIFIED_WATER_BOTTLE).input(WHEAT, 2).input(RED_MUSHROOM).criterion(RecipeProvider.hasItem(DrinkableItems.PURIFIED_WATER_BOTTLE), RecipeProvider.conditionsFromItem(DrinkableItems.PURIFIED_WATER_BOTTLE)).offerTo(exporter, RecipeProvider.getItemPath(DrinkableItems.WHEAT_EXTRACT) + "_from_red_mushroom");
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.FOOD, DrinkableItems.WHEAT_EXTRACT, 1).input(DrinkableItems.PURIFIED_WATER_BOTTLE).input(WHEAT, 2).input(BROWN_MUSHROOM).criterion(hasItem(DrinkableItems.PURIFIED_WATER_BOTTLE), RecipeProvider.conditionsFromItem(DrinkableItems.PURIFIED_WATER_BOTTLE)).offerTo(exporter, RecipeProvider.getItemPath(DrinkableItems.WHEAT_EXTRACT) + "_from_brown_mushroom");
-        RecipeProvider.offerFoodCookingRecipe(exporter, "drink_fermenter", RecipeSerializer.CAMPFIRE_COOKING, 600, DrinkableItems.WHEAT_EXTRACT, DrinkableItems.WHEAT_BEER, 1f);
+        RecipeProvider.offerSmelting(
+                exporter,
+                List.of(Items.POTION),
+                RecipeCategory.FOOD,
+                DrinkableItems.PURIFIED_WATER_BOTTLE,
+                0.1f,
+                200,
+                "tempus");
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.FOOD, DrinkableItems.WHEAT_EXTRACT, 1)
+                .input(DrinkableItems.PURIFIED_WATER_BOTTLE)
+                .input(WHEAT, 2)
+                .input(RED_MUSHROOM)
+                .criterion(
+                        RecipeProvider.hasItem(DrinkableItems.PURIFIED_WATER_BOTTLE),
+                        RecipeProvider.conditionsFromItem(DrinkableItems.PURIFIED_WATER_BOTTLE))
+                .offerTo(exporter, RecipeProvider.getItemPath(DrinkableItems.WHEAT_EXTRACT) + "_from_red_mushroom");
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.FOOD, DrinkableItems.WHEAT_EXTRACT, 1)
+                .input(DrinkableItems.PURIFIED_WATER_BOTTLE)
+                .input(WHEAT, 2)
+                .input(BROWN_MUSHROOM)
+                .criterion(
+                        hasItem(DrinkableItems.PURIFIED_WATER_BOTTLE),
+                        RecipeProvider.conditionsFromItem(DrinkableItems.PURIFIED_WATER_BOTTLE))
+                .offerTo(exporter, RecipeProvider.getItemPath(DrinkableItems.WHEAT_EXTRACT) + "_from_brown_mushroom");
+        RecipeProvider.offerFoodCookingRecipe(
+                exporter,
+                "drink_fermenter",
+                RecipeSerializer.CAMPFIRE_COOKING,
+                600,
+                DrinkableItems.WHEAT_EXTRACT,
+                DrinkableItems.WHEAT_BEER,
+                1f);
     }
 }
